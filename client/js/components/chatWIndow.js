283b604708ff916df1d988229e1d751016ecfd7d
Minato Namikaze <minatoofficials08@gmail.com>
2026-07-28 02:50:35 +0545

    Add chatWindow component for chat interface

---
+++ client/js/components/chatWIndow.js
@@ -0,0 +1,93 @@
+export default function chatWindow() {
+
+    const element = document.createElement("div");
+
+    element.className = "chat-window";
+
+
+    element.innerHTML = `
+
+        <div class="chat-header">
+
+
+            <div class="chat-user">
+
+
+                <div class="chat-avatar online">
+                    A
+                </div>
+
+
+                <div>
+
+                    <h3>
+                        Alex
+                    </h3>
+
+                    <span>
+                        Online
+                    </span>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+
+
+        <div class="messages">
+
+
+            <div class="message received">
+
+                Hey, what's up?
+
+            </div>
+
+
+
+            <div class="message sent">
+
+                Nothing much, just testing ChatVerse.
+
+            </div>
+
+
+
+            <div class="message received">
+
+                Nice! Looks cool.
+
+            </div>
+
+
+        </div>
+
+
+
+
+        <div class="message-input">
+
+
+            <input 
+                placeholder="Type a message..."
+            >
+
+
+            <button>
+                ➤
+            </button>
+
+
+        </div>
+
+
+    `;
+
+
+    return element;
+
+}
