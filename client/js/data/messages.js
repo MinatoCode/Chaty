d4f33d8c4882e83da41c702c5c472b190c4b1c3c
Minato Namikaze <minatoofficials08@gmail.com>
2026-07-29 01:47:49 +0545

    Create messages.js

---
+++ client/js/data/messages.js
@@ -0,0 +1,16 @@
+export const messages = [
+    {
+        id: 1,
+        sender: "Alex",
+        text: "Hey! 👋",
+        time: "10:15 PM",
+        own: false
+    },
+    {
+        id: 2,
+        sender: "You",
+        text: "Hello!",
+        time: "10:16 PM",
+        own: true
+    }
+];
